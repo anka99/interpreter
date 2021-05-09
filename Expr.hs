5efd32aab120d3469cf8556487b486beb92d59de
Anna <ak406173@students.mimuw.edu.pl>
2021-05-09 18:56:51 +0200

    Accessing value in store, throwing error

--- Expr.hs
+++ Expr.hs
@@ -8,6 +8,11 @@ import ParEmm
 import PrintEmm
 import SkelEmm ()
 
+import Control.Monad.Except
+import Control.Monad.State
+import System.IO
+import Control.Monad.Reader
+
 import Prelude
 
 eval :: Expr -> TurboMonad Value
@@ -21,11 +26,15 @@ eval (EMul pos e1 op e2) = do
 eval (EAdd pos e1 op e2) = do
   IntVal v1 <- eval e1
   IntVal v2 <- eval e2
-  return $ IntVal $ evalAddOp v1 v2
+  return $ IntVal $ evalAddOp op v1 v2
 
 eval (Neg pos e) = do
   IntVal v <- eval e
   return  $ IntVal $ (-1) * v
+
+eval (EVar pos ident) = getLoc ident >>= getVal
+  -- return $ lookup (lookup ident env) store
+
 
 evalMulOp :: MulOp -> Integer -> Integer -> Integer
 evalMulOp (Times pos) = (*)
--- Turbo.hs
+++ Turbo.hs
@@ -17,7 +17,7 @@ type Loc = Int
 type Env = Map.Map Ident Loc
 type Store = Map.Map Loc Value
 
--- TODO : być może coś zmienić, bo Mr. C.
+-- TODO : być może coś zmienić, bo Mr. C.env
 type TurboMonad a = ReaderT Env (ExceptT String (StateT Store IO)) a
 
 runTurbo :: Env -> Store -> TurboMonad a -> IO (Either String a, Store)
@@ -25,3 +25,28 @@ runTurbo env store e = runStateT (runExceptT (runReaderT e env)) store
 
 changeEnvTo :: Env -> Env -> Env
 changeEnvTo to from = to
+
+getLoc :: Ident -> TurboMonad Loc
+getLoc ident = do
+  env <- ask
+  case Map.lookup ident env of
+    Just loc -> return loc
+    Nothing -> throwError "Undeclared variable"
+
+-- TODO: remove "fucking"
+getVal :: Loc -> TurboMonad Value
+getVal loc = do
+  store <- get
+  case Map.lookup loc store of
+    Just NullVal -> throwError "Uninitialized value"
+    Just val -> return val
+    Nothing -> throwError "This should have never happend and if did we can safely assume, that author of this code is fucking stupid."
+
+-- getValue :: Loc -> TurboMonad (Maybe Value)
+-- getValue loc = return $ get >>= lookup loc
+
+-- getVal :: Ident -> TurboMonad (Maybe Value)
+-- getVal ident = do
+--   store <- get
+--   env <- ask
+--   return $ lookup (lookup )
