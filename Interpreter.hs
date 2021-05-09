46e29ba1f07fc504eb6a1ca416c5a78110328be7
Anna <ak406173@students.mimuw.edu.pl>
2021-05-09 12:02:54 +0200

    Evaluating simple expressions using transformed monad. Working code : int x = 3 + 4;

--- Interpreter.hs
+++ Interpreter.hs
@@ -19,12 +19,12 @@ import SkelEmm ()
 type Err        = Either String
 type ParseFun a = [Token] -> Err a
 
-data Value = ValInt Integer | ValBool Bool | ValString String | ValFunc Env [Arg] Block | ValNull
-    deriving (Eq, Ord)
+data Value = IntVal Integer | BoolVal Bool | StringVal String | FuncVal Env [Arg] Block | NullVal
+    deriving (Eq, Ord, Show)
 
 type Loc = Int
 type Env = Map Ident Loc
-type Store = Map Loc Ident
+type Store = Map Loc Value
 
 -- TODO : być może coś zmienić, bo Mr. C.
 type TurboMonad a = ReaderT Env (ExceptT String (StateT Store IO)) a
@@ -40,35 +40,69 @@ interpret content =
       exitFailure
     Right tree -> do
       putStrLn "\nParse Successful!"
-      (res, store) <- runTurbo empty empty $ interpretProgram tree
-      putStrLn $ show res
+      (env, store) <- runTurbo empty empty (runProgram tree)
+      putStrLn $ show store
+      putStrLn $ show env
       -- exitWith interpretProgram tree
   where
   parsedContent = pProgram $ myLexer content
 
-interpretProgram :: Program -> TurboMonad Integer
-interpretProgram (Program pos decl) = do
-  env <- interpretDeclList decl
-  return 0
+runProgram :: Program -> TurboMonad (Env, Store)
+runProgram (Program pos decls) = do
+  env <- interpretDeclList decls
+  store <- get
+  return (env, store)
 
-newLoc :: Store -> Loc
-newLoc s = size s
+changeEnvTo :: Env -> Env -> Env
+changeEnvTo to from = to
 
--- TODO capp
 interpretDeclList :: [Decl] -> TurboMonad Env
 interpretDeclList [] = ask
 interpretDeclList (d:l) = do
   env <- interpretDecl d
-  local (const env) $ interpretDeclList l
+  local (changeEnvTo env) $ interpretDeclList l
 
 interpretDecl :: Decl -> TurboMonad Env
-interpretDecl (Decl pos t []) = ask
-interpretDecl (Decl pos t (i:l)) = do
-  env <- declareItem i t
-  local (const env) $ interpretDecl $ Decl pos t l
+interpretDecl (Decl pos t items) = addItemList items
 
-declareItem :: Item -> Type -> TurboMonad Env
-declareItem (NoInit pos ident) t = do
-  store <- get
+addItemList :: [Item] -> TurboMonad Env
+addItemList [] = ask
+addItemList (i:l) = do
+  env <- addItem i
+  local (changeEnvTo env) $ addItemList l
+
+newLoc :: Store -> Loc
+newLoc s = size s
+
+addItemVal :: Ident -> Value -> TurboMonad Env
+addItemVal ident val = do
   env <- ask
+  store <- get
+  put $ insert (newLoc store) val store
   return $ insert ident (newLoc store) env
+
+addItem :: Item -> TurboMonad Env
+addItem (NoInit pos ident) = addItemVal ident NullVal
+addItem (Init pos ident expr) = eval expr >>= addItemVal ident
+-- TODO array
+
+intValArithm :: (Integer -> Integer -> Integer) -> Value -> Value -> TurboMonad Value
+intValArithm fun (IntVal v1) (IntVal v2) = return $ IntVal $ fun v1 v2
+
+
+eval :: Expr -> TurboMonad Value
+eval (ELitInt pos x) = return $ IntVal x
+
+eval (EMul pos e1 op e2) = do
+  IntVal v1 <- eval e1
+  IntVal v2 <- eval e2
+  return $ IntVal $ v1 * v2
+
+eval (EAdd pos e1 op e2) = do
+  IntVal v1 <- eval e1
+  IntVal v2 <- eval e2
+  return $ IntVal $ v1 + v2
+
+eval (Neg pos e) = do
+  IntVal v <- eval e
+  return  $ IntVal $ (-1) * v
